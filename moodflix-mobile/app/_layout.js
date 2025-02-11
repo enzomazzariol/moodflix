e0ae953c297c9a063d4f4f7aab6b9d2ab6e00f5b
mazzariolenzo <mazzariolenzo@gmail.com>
2025-02-11 17:41:58 +0100

    add animation to splash

--- moodflix-mobile/app/_layout.js
+++ moodflix-mobile/app/_layout.js
@@ -1,9 +1,9 @@
 import { Stack, router } from 'expo-router';
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar, Animated, Easing } from 'react-native';
 import '../global.css';
-import { StatusBar } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -17,36 +17,46 @@ export default function RootLayout() {
   //TODO: Obtener usuario autenticado con un useEffect
   // SI esta logeado lo redirigimos al (tabs) sino al auth (login/sign up)
   const[isReady, setIsReady] = useState(false);
+  const fadeAnim = useRef(new Animated.Value(0)).current; 
   // Falta estado para autenticacion
 
   useEffect(() => {
     const checkAuth = async () => {
       // Simulación de carga de datos para PRUEBA
-      await new Promise(resolve => setTimeout(resolve, 2000)); // Simula un delay de carga
+      await new Promise(resolve => setTimeout(resolve, 1500)); // Simula un delay de carga
       setIsReady(true);
     };
 
     checkAuth();
   }, [])
 
-  const OnLayoutRootView = useCallback(async () => {
-    if(isReady) {
-      await SplashScreen.hideAsync();
-      router.replace('(tabs)')
-       // router.replace(isAuthenticated ? "(tabs)" : "(auth)");
+  useEffect(() => {
+    if (isReady) {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000, 
+        easing: Easing.out(Easing.quad),
+        useNativeDriver: true,
+        delay: 200
+      }).start(async () => {
+        await SplashScreen.hideAsync();
+        router.replace('(tabs)');
+      });
     }
-  }, [isReady])
+  }, [isReady]);
 
   if(!isReady) {
     return null;
   }
 
   return (
-    <SafeAreaProvider onLayout={OnLayoutRootView}>
+    <SafeAreaProvider>
       <StatusBar style="auto" />
+      <Animated.View style={{ flex: 1, opacity: fadeAnim }}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
+      </Animated.View>
     </SafeAreaProvider>
   )
 }
