375015d27c865362b019ee8271bb20b3729e6bbf
mazzariolenzo <mazzariolenzo@gmail.com>
2025-02-20 17:20:06 +0100

    fade animation between screens

--- moodflix-mobile/src/app/(tabs)/index.js
+++ moodflix-mobile/src/app/(tabs)/index.js
@@ -1,10 +1,17 @@
-import { Text } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import TabsScreen from "../../components/screens/TabsScreen";
+import { Link } from "expo-router";
 
 export default function Home() {
+  
   return (
     <TabsScreen>
-      <Text className="text-stone-100 text-3xl">Home</Text>
+        <Text className="text-stone-100 text-3xl">Home</Text>
+        <Link href='/(auth)/login' asChild>
+          <TouchableOpacity className="p-3 rounded-full bg-stone-500 shadow-md">
+            <Text>Ir al login</Text>
+          </TouchableOpacity>
+        </Link>
     </TabsScreen>
-  )
+  );
 }
--- moodflix-mobile/src/app/_layout.js
+++ moodflix-mobile/src/app/_layout.js
@@ -1,17 +1,44 @@
-import { Stack } from 'expo-router';
+import { useEffect, useState } from "react";
+import { Stack, useRouter } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StatusBar } from 'react-native';
-import '../../global.css';
+import { StatusBar, View, ActivityIndicator } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
+import "../../global.css";
+
+SplashScreen.preventAutoHideAsync(); 
 
 export default function RootLayout() {
+  const router = useRouter();
+  const [isReady, setIsReady] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    const prepareApp = async () => {
+      // Simulación de carga (puedes cargar fuentes, datos, etc.)
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      setIsAuthenticated(true); 
+      setIsReady(true);
+    };
+
+    prepareApp();
+  }, []);
+
+   useEffect(() => {
+    if (isReady) {
+      SplashScreen.hideAsync();
+      router.replace(isAuthenticated ? "/(tabs)" : "/(auth)/login");
+    }
+  }, [isReady]);
   return (
     <SafeAreaProvider>
-      <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <StatusBar style="auto" />
+        <Stack screenOptions={{ headerShown: false, animation: "fade_from_bottom" }}>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
     </SafeAreaProvider>
-  )
+  );
 }
