40279da675dd5d4b4c64881d268d612aaedd3125
mazzariolenzo <mazzariolenzo@gmail.com>
2025-03-27 12:24:56 +0100

    endpoints de get users and user by id

--- backend/src/main/java/com/moodflix/backend/controller/UserController.java
+++ backend/src/main/java/com/moodflix/backend/controller/UserController.java
@@ -1,18 +1,42 @@
 package com.moodflix.backend.controller;
 
+import com.moodflix.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 @RequestMapping("/moodflix")
+@CrossOrigin("*")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/getApp")
     public ResponseEntity<?> getHealth(){
         return ResponseEntity.ok("Moodflix API is running!");
+    }
+
+    /**
+     * Endpoint para obtener a todos los usuarios
+     */
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers() {
+        return userService.getUsers();
+    }
+
+    /**
+     * Endpoint para obtener a un usuario por ID
+     */
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUsers(@PathVariable int id) {
+        return userService.getUserById(id);
     }
 }
